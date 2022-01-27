bb19d9df400594b7521523c6bcf07669356e4249
Sean <seanhoyeonjin@gmail.com>
2022-01-26 19:33:20 -0500

    removed yarn.lock

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,11 +1,9 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData(value) {
   // manage all the calls in a state
   const [allCalls, setAllCalls] = useState([]);
-  const [unarchivedCalls, setUnarchivedCalls] = useState([]);
-  const [archivedCalls, setArchivedCalls] = useState([]);
 
   // make a get request to the api to retrive all the calls
   // this useEffect is triggered once again as the "value" changes which is a hook that changes state once user archives or un-archives
